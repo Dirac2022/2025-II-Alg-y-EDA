2bc755f7392f2dd80722b164b6579a9324476200
Dirac <daronmitchel@gmail.com>
2025-10-14 19:49:25 -0500

    add rotate left and right methods

--- avl.h
+++ avl.h
@@ -4,28 +4,28 @@
 #include "binarytree.h"
 
 template <typename Traits>
-class CAVLNode : public CBinaryTreeNode<value_type>{
+class CAVLNode : public CBinaryTreeNode<Traits>{
 public:
   using value_type = typename Traits::T;
-  using Node       = CBinaryTreeNode<value_type>;
+  using Node       = CBinaryTreeNode<Traits>;
 protected:
     int     m_balanceFactor = 0; // Balance factor for AVL tree
 public:
 };
 
-template <typename _T>
-struct AVLAscTraits{
-    using  value_type = _T;
-    using  Node       = CAVLNode<T>;
-    using  CompareFn  = less<T>;
-};
+// template <typename _T>
+// struct AVLAscTraits{
+//     using  value_type = _T;
+//     using  Node       = CAVLNode<T>;
+//     using  CompareFn  = less<T>;
+// };
 
-template <typename _T>
-struct AVLDescTraits{
-    using  value_type = _T;
-    using  Node       = CAVLNode<T>;
-    using  CompareFn  = greater<T>;
-};
+// template <typename _T>
+// struct AVLDescTraits{
+//     using  value_type = _T;
+//     using  Node       = CAVLNode<T>;
+//     using  CompareFn  = greater<T>;
+// };
 
 template <typename Traits>
 class CAVLTree : public CBinaryTree<Traits> {
@@ -50,6 +50,85 @@ protected:
     }
 public:
     CAVLTree() : Base() {} // Empty tree
+
+    void insert(value_type elem, Ref ref) {
+        bool height_grew = false;
+        internal_insert(elem, ref, nullptr, this->m_pRoot, height_grew);
+    }
+
+private:
+    Node* CreateNode(Node* pParent, value_type elem, Ref ref) override {
+        return new Node(pParent, elem, ref);
+    }
+
+    void rotateRight(Node*& pNode) {
+        Node* pChild = pNode->getChild(0); // hijo izquierdo (pChild) pasara a ser padre de pNode
+        pChild->m_pParent = pNode->m_pParent; // padre de pChild sera ahora padre de pNode
+        pNode->setPChild(pChild->getChild(1), 0); // hijo iz de pNode ahora sera sera el hijo derecho de pChild
+        if (pNode->getChild(0)) { // Si habia hijo derecho de pchild actualiza su padre, ahora pNode
+            pNode->getChild(0)->m_pParent = pNode;
+        }
+        pChild->setpChild(pNode, 1); // pNode ahora hijo iz de pChild (antiguo hijo iz ahora hijo de de pNode)
+        pNode->m_pParent = pChild; 
+        pNode = pChild; // pParent de pNode ahora apunta a un difente hijo (pChild)
+    }
+
+    void rorateLeft(Node*& pNode) {
+        Node* pChild = pNode->getChild(1); // hijo der (pChild) pasara a ser padre de pNode
+        pChild->m_pParent = pNode->m_pParent; // padre de pChild sera ahora padre de pNode
+        pNode->setPChild(pChild->getChild(0), 1); // hijo der de pNode ahora sera sera el hijo iz de pChild
+        if (pNode->getChild(1)) { // Si habia hijo izq de pchild actualiza su padre, ahora pNode
+            pNode->getChild(1)->m_pParent = pNode;
+        }
+        pChild->setpChild(pNode, 0);
+        pNode->m_pParent = pChild;
+        pNode = pChild;
+    }
+
+    void balance(Node*& pNode) {
+        if (pNode->m_balanceFactor > 1) {
+            Node* pChild = pNode->getChild(1);
+            if (pChild->m_balanceFactor < 0) // RL
+                rotateRight(pNode->getChildRef(1));
+            rotateLeft(pNode); // RR
+        }
+        else if (pNode->m_balanceFactor < -1) {
+            Node* pChild = pNode->getChild(0);
+            if (pChild->m_balanceFactor > 0) // LR
+                rotateLeft(pNode->getChildRef(0));
+            rotateRight(pNode); // LL
+        }
+
+        pNode->m_balanceFactor = 0;
+        if (pNode->getChild(0))
+            pNode->getChild(0)->m_balaceFactor = 0;
+        if (pNode->getChiild(1))
+            pNode->getChild(1)->m_balanceFactor = 0;
+    }
+
+    void internal_insert(value_type& elem, Ref ref, Node* pParent, 
+                         Node*& rpOrigin, bool& height_grew) override
+    {
+        if (!rpOrigin) {
+            rpOrigin = CreateNode(pParent, elem, ref);  
+            this->m_size ++;
+            height_grew = true;
+            return;
+        }
+
+        size_t branch = this->Compfn(elem, rpOrigin->getDataRef()) ? 0 : 1;
+        internal_insert(elem, ref, rpOrigin, rpOrigin->getChildRef(branch), height_grew);
+
+        if (height_grew) {
+            rpOrigin->m_balanceFactor += (branch == 0 ? -1 : 1);
+            if (rpOrigin->m_balanceFactor == 0) 
+                height_grew = false;
+            else if (abs(rpOrigin->m_balanceFactor) > 1) {
+                balance(rpOrigin);
+                height_grew = false;
+            }
+        }
+    }
 
 };
 
